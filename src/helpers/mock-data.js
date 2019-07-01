92438f051e4bf1bb6e4edeadb54849c03ee70745
gusadolfo123 <gusadolfo123@hotmail.com>
2019-07-01 18:59:32 -0500

    Ajuste fincas

--- src/helpers/mock-data.js
+++ src/helpers/mock-data.js
@@ -2,13 +2,72 @@ import faker from 'faker/locale/es_MX';
 import { Farm } from '../models/farm';
 import { Service } from '../models/service';
 import { connect } from '../database';
+import { Company } from '../models/company';
+import { Seasons } from '../models/seasons';
 
 const mockData = {};
 
 mockData.generateFarms = async (req, res) => {
 	let listServices = [];
 	let farms = [];
+
 	const db = await connect();
+
+	//company
+	const company = new Company({
+		name: faker.company.companyName(),
+		dir: faker.address.streetAddress(true),
+		coordinate: {
+			lat: faker.address.latitude(),
+			lon: faker.address.longitude(),
+		},
+		phones: [
+			{
+				phone_type: 'local',
+				number: faker.phone.phoneNumber(),
+			},
+			{
+				phone_type: 'local',
+				number: faker.phone.phoneNumber(),
+			},
+			{
+				phone_type: 'celular',
+				number: faker.phone.phoneNumber(),
+			},
+			{
+				phone_type: 'celular',
+				number: faker.phone.phoneNumber(),
+			},
+		],
+		whatsapp: faker.phone.phoneNumber(),
+		images: [
+			{
+				name: faker.image.nature.name,
+				url: 'https://picsum.photos/1920/800/?image=' + faker.random.number(1084), // faker.image.imageUrl(1920, 800, 'nature', true, true),
+				size: 9545493.45,
+			},
+			{
+				name: faker.image.nature.name,
+				url: 'https://picsum.photos/1920/800/?image=' + faker.random.number(1084), // faker.image.imageUrl(1920, 800, 'nature', true, true),
+				size: 9545493.45,
+			},
+			{
+				name: faker.image.nature.name,
+				url: 'https://picsum.photos/1920/800/?image=' + faker.random.number(1084), // faker.image.imageUrl(1920, 800, 'nature', true, true),
+				size: 9545493.45,
+			},
+			{
+				name: faker.image.nature.name,
+				url: 'https://picsum.photos/1920/800/?image=' + faker.random.number(1084), // faker.image.imageUrl(1920, 800, 'nature', true, true),
+				size: 9545493.45,
+			},
+		],
+		mission: faker.company.bsAdjective(),
+		vision: faker.company.bsBuzz(),
+		description: faker.lorem.paragraphs(5),
+	});
+
+	await db.collection('companies').insertOne(company);
 
 	// services
 	for (let index = 0; index < 5; index++) {
@@ -20,7 +79,7 @@ mockData.generateFarms = async (req, res) => {
 		listServices.push(newService);
 	}
 
-	db.collection('services').insertMany(listServices);
+	await db.collection('services').insertMany(listServices);
 
 	for (let index = 0; index < 10; index++) {
 		const newFarm = new Farm({
@@ -59,18 +118,28 @@ mockData.generateFarms = async (req, res) => {
 				},
 			},
 			services: [listServices[0]._id.toString(), listServices[1]._id.toString()],
-			terms_conditions: [
-				faker.random.words(5),
-				faker.random.words(5),
-				faker.random.words(5),
-				faker.random.words(5),
-				faker.random.words(5),
-			],
+			terms_conditions: [faker.random.words(5), faker.random.words(5), faker.random.words(5), faker.random.words(5), faker.random.words(5)],
 		});
 		farms.push(newFarm);
 	}
+
 	await db.collection('farms').insertMany(farms);
-	res.status(200).json(farms);
+
+	const seasons = new Seasons({
+		mid: [faker.date.future(), faker.date.future(), faker.date.future(), faker.date.future(), faker.date.future(), faker.date.future()],
+		hight: [faker.date.future(), faker.date.future(), faker.date.future(), faker.date.future(), faker.date.future(), faker.date.future()],
+	});
+
+	await db.collection('seasons').insertMany(seasons);
+
+	const result = {
+		company,
+		listServices,
+		farms,
+		seasons,
+	};
+
+	res.status(200).json(result);
 };
 
 export default mockData;
