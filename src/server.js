0e2b138c22c129eb7d89a3be6fbae81896ad1901
gusadolfo123 <gusadolfo123@hotmail.com>
2019-07-23 16:48:41 -0500

    Implementacion logs

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import express, { json, urlencoded, Router } from 'express';
 import MapRoutes from './routes/main.routes';
+import morgan from 'morgan';
+let winston = require('../winston');
 
 const app = express();
 
 //Config
 app.set('port', process.env.PORT || 4565);
+
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
 
 //Middlewares
 app.use(urlencoded({ extended: false }));
@@ -12,5 +25,7 @@ app.use(json()); // para capturar datos que se envian en el cuerpo de un request
 
 //Map Routes
 MapRoutes(app, Router());
+
+app.use(morgan('combined', { stream: winston.stream }));
 
 export default app;
